ff5cac6a249517ea62d7f254a9492ec58b6eebba
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-05-21 18:56:35 +0100

    searchbox and giphy website button working

--- public/app.js
+++ public/app.js
@@ -1,5 +1,44 @@
 var app = function() {
+  var url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+  bindEvents();
+  makeRequest("GET", url, requestComplete);
+}
 
+var bindEvents = function(){
+  var searchQuery = document.getElementById("search-query");
+  var gifsDiv = document.getElementById("gifs");
+
+  searchQuery.addEventListener("keyup", function(){
+    gifsDiv.innerHTML = "";
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + this.value + "&api_key=dc6zaTOxFJmzC";
+    makeRequest("GET", url, requestComplete);
+  });
+}
+
+var makeRequest = function(method, url, callback){
+  var request = new XMLHttpRequest();
+  request.open(method, url);
+  request.addEventListener("load", callback);
+  request.send();
+}
+
+var requestComplete = function(){
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var data = JSON.parse(jsonString);
+  display(data);
+}
+
+var display = function(data){
+  console.log(data);
+  var gifData = document.getElementById("gifs");
+
+  data.data.forEach(function(gif){
+    var div = document.createElement("div");
+    div.className = "gif";
+    div.innerHTML = "<img src='" + gif.images.fixed_height_downsampled.url + "'>"
+    gifData.appendChild(div);
+  });
 }
 
 window.addEventListener("load", app);
